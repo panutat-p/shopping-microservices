602b4c04fca4778a924512d17cae87094c81694f
panutat-p <pp.panutat@gmail.com>
2023-03-25 16:10:30 +0700

    feat: GET user, POST register, POST login

--- auth-express/routes/users.js
+++ auth-express/routes/users.js
@@ -1,8 +1,22 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  return res.status(200).json({
+    message: 'User',
+  });
+});
+router.post('/register', function (req, res, next) {
+  return res.status(200).json({
+    message: 'Register successfully',
+  });
+});
+
+router.post('/login', function (req, res, next) {
+  return res.status(200).json({
+    message: 'token',
+  });
 });
 
 module.exports = router;
