04daf04df34cf7c75c640aef049cc7118a6c97de
panutat-p <pp.panutat@gmail.com>
2023-04-01 10:20:09 +0700

    fix: middleware must return on next()

--- product-express/middlewares/check-admin.js
+++ product-express/middlewares/check-admin.js
@@ -2,13 +2,13 @@ module.exports.checkAdminRole = (req, res, next) => {
   try {
     const { role } = req.user;
     if (role === 'admin') {
-      next();
+      return next();
     }
 
     return res.status(403).json({
       message: 'Unauthorized, role:' + role,
     });
   } catch (e) {
-    next(e);
+    return next(e);
   }
 };
