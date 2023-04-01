ac74b4d6aef2ff04966d9a62f295b4a21e457890
panutat-p <pp.panutat@gmail.com>
2023-04-01 12:12:27 +0700

    feat: load ENV

--- order-gin/main.go
+++ order-gin/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("🟥 Failed load ENV, err:", err)
+		os.Exit(1)
+	}
+
+	gin.SetMode(os.Getenv("GIN_MODE"))
 	r := gin.Default()
 	r.GET("/ping", PingPong)
 	r.GET("/api/v1/orders", ListOrders)
-	err := r.Run(":4002")
+
+	err = r.Run(":" + os.Getenv("PORT"))
 	if err != nil {
 		fmt.Println("🟥 Failed to start Gin, err:", err)
 		return
